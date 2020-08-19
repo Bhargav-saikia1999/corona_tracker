9b6bd7e28beb90d8b41db589596ebf9b61c62f73
bhargav-saikia <bhargav.saikia@iitg.ac.in>
2020-08-20 00:29:47 +0530

    refactor css & js, cards styling changed

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const card3 = document.querySelector(".card3 .value3");
 const cards = document.querySelectorAll(".crd");
 const error = document.querySelector(".error");
 
+const crdValueArray = [card1, card2, card3];
+
 btn.addEventListener("click", (e) => {
   e.preventDefault();
 
   cards.forEach((cardd) => {
     if (cardd.classList.contains("show")) {
       cardd.classList.remove("show");
+      cardd.classList.remove("animate");
     }
   });
 
@@ -24,45 +27,56 @@ btn.addEventListener("click", (e) => {
 
   const url = "https://api.covid19india.org/data.json";
 
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      let stateArray = data.statewise;
-      var count = 0;
+  setTimeout(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        let stateArray = data.statewise;
+        var count = 0;
 
-      for (var st of stateArray) {
-        if (st.state === userInput) {
-          card1.textContent = st.confirmed;
-          card2.textContent = st.deaths;
-          card3.textContent = st.active;
-          cards.forEach((cardd) => {
-            cardd.classList.add("show");
-          });
-          break;
+        for (var st of stateArray) {
+          if (st.state === userInput) {
+            card1.textContent = st.confirmed;
+            card2.textContent = st.deaths;
+            card3.textContent = st.active;
+            cards.forEach((cardd) => {
+              cardd.classList.add("show");
+              cardd.classList.add("animate");
+            });
+            break;
+          }
+          count++;
         }
-        count++;
-      }
 
-      if (count >= stateArray.length) {
-        error.innerHTML = `<h4>SORRY DATA NOT AVAILABLE FOR ${userInput}!</h4>
+        if (count >= stateArray.length) {
+          error.innerHTML = `<h4>SORRY DATA NOT AVAILABLE FOR ${userInput}!</h4>
         <ul style="text-align: left; list-style:none;"><li>1) Eg. for one-worded states type- Assam</li>
         <li>2) Eg. for two-worded states type- Tamil Nadu</li>
         <li>3) Try another spelling.</li>
         </ul>
         `;
-        error.style.padding = "0.8rem";
-        // const para = document.createElement("p");
-        // para.innerHTML = "Tip: try another spelling !";
-        // error.appendChild(para);
+          error.style.padding = "0.8rem";
+          // const para = document.createElement("p");
+          // para.innerHTML = "Tip: try another spelling !";
+          // error.appendChild(para);
 
-        setTimeout(() => {
-          error.innerHTML = ``;
-          error.style.padding = "0rem";
-        }, 10000);
-      }
-    });
+          setTimeout(() => {
+            error.innerHTML = ``;
+            error.style.padding = "0rem";
+          }, 5000);
+        }
+      });
+  }, 300);
 
   input.value = "";
 });
 
-//
+// crdValueArray.forEach((item) => {
+//   item.addEventListener("change", (e) => {
+//     cards.forEach((cardd) => {
+//       if (cardd.classList.contains("show")) {
+//         cardd.classList.add("animate");
+//       }
+//     });
+//   });
+// });
